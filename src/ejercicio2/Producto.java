d1ef3e1540adfeb5491b200986090a3223bd7742
Mañana <Maana@DESKTOP-RCDFL5S>
2024-04-16 10:17:44 +0200

    La solucion es poner el calculo en otra clase.

--- src/ejercicio2/Producto.java
+++ src/ejercicio2/Producto.java
@@ -2,12 +2,5 @@ package ejercicio2;
 
 public class Producto {
 
-    public void agregarProducto(Producto producto) { 
-        // Lógica para agregar un producto
-    } 
-    
-    public void eliminarProducto(Producto producto) { 
-        // Lógica para eliminar un producto 
-    } 
 	
 }
