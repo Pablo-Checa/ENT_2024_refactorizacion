53a3c2fa2487a1ddcade07302942a6193dc76fda
Mañana <Maana@DESKTOP-RCDFL5S>
2024-04-16 09:29:44 +0200

    Solucion mala

--- src/ejercicio1/Calculadora.java
+++ src/ejercicio1/Calculadora.java
@@ -2,37 +2,43 @@ package ejercicio1;
 
 public class Calculadora {
 
-	public int sumar(int a, int b) {
-		int resultado = a + b;
-		return resultado;
+		private int a;
+		private int b;
+	
+	    public int sumar(int a, int b) {
+	        int resultado = a + b;
+	        return resultado;
+	    }
 
+	    public int restar(int a, int b) {
+	        int resultado = a - b;
+	        return resultado;
+	    }
+
+	    public int multiplicar(int a, int b) {
+	        int resultado = a * b;
+	        return resultado;
+	    }
+
+	    public int dividir(int a, int b) {
+	        if (b == 0) {
+	            System.out.println("Error: No se puede dividir por cero.");
+	            return 0;
+	        } else {
+	            int resultado = a / b;
+	            return resultado;
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        Calculadora calc = new Calculadora();
+
+	        System.out.println("Suma: " + calc.sumar(10, 5));
+	        System.out.println("Resta: " + calc.restar(10, 5));
+	        System.out.println("Multiplicación: " + calc.multiplicar(10, 5));
+	        System.out.println("División: " + calc.dividir(10, 5));
+	    }
 	}
 
-	public int restar(int a, int b) {
-		int resultado = a - b;
-		return resultado;
+	
 
-	}
-
-	public int multiplicar(int a, int b) {
-		int resultado = a * b;
-		return resultado;
-
-	}
-
-	public int dividir(int a, int b) {
-
-		if (b == 0) {
-			System.out.println("Error: No se puede dividir por cero.");
-			return 0;
-
-		} else {
-			
-			int resultado = a / b;
-			return resultado;
-
-		}
-
-	}
-
-}
