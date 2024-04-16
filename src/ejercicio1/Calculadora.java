1245f27495fe2fbb95cc42655d571b4bf63b2f38
Mañana <Maana@DESKTOP-RCDFL5S>
2024-04-16 09:19:12 +0200

    Código original

--- src/ejercicio1/Calculadora.java
+++ src/ejercicio1/Calculadora.java
@@ -2,4 +2,37 @@ package ejercicio1;
 
 public class Calculadora {
 
+	public int sumar(int a, int b) {
+		int resultado = a + b;
+		return resultado;
+
+	}
+
+	public int restar(int a, int b) {
+		int resultado = a - b;
+		return resultado;
+
+	}
+
+	public int multiplicar(int a, int b) {
+		int resultado = a * b;
+		return resultado;
+
+	}
+
+	public int dividir(int a, int b) {
+
+		if (b == 0) {
+			System.out.println("Error: No se puede dividir por cero.");
+			return 0;
+
+		} else {
+			
+			int resultado = a / b;
+			return resultado;
+
+		}
+
+	}
+
 }
