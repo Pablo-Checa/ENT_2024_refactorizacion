5bf46ae46718755a4c9bb043b888ffc715d38a66
Mañana <Maana@DESKTOP-RCDFL5S>
2024-04-23 08:51:38 +0200

    Solucion MIA Ejercicio 4 Ejemplo 1

--- src/ejercicio4/Calculadora.java
+++ src/ejercicio4/Calculadora.java
@@ -2,4 +2,20 @@ package ejercicio4;
 
 public class Calculadora {
 
+	public static final int CODIGO_SUMA = 0;
+	public static final int CODIGO_RESTA = 1;
+
+	public int operacionAritmetica(int a, int b) {
+
+		int tipoOperacion = CODIGO_SUMA;
+		int c = 0;
+		if (tipoOperacion == CODIGO_SUMA) {
+			c = a + b;
+		} else {
+			c = a - b;
+		}
+		return c;
+
+	}
+
 }
