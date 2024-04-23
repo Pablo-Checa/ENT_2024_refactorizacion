2050f820182220bdcde14efd0a016aed2d57b00e
Mañana <Maana@DESKTOP-RCDFL5S>
2024-04-23 09:01:44 +0200

    RESULTADO FINAL

--- src/ejercicio4/Calculadora.java
+++ src/ejercicio4/Calculadora.java
@@ -2,20 +2,29 @@ package ejercicio4;
 
 public class Calculadora {
 
+	/**
+	 * Atributos estáticos que nos indican el significado de la operación que pongamos.
+	 */
 	public static final int CODIGO_SUMA = 0;
 	public static final int CODIGO_RESTA = 1;
 
-	public int operacionAritmetica(int a, int b) {
-
-		int tipoOperacion = CODIGO_SUMA;
-		int c = 0;
-		if (tipoOperacion == CODIGO_SUMA) {
-			c = a + b;
-		} else {
-			c = a - b;
+	/**
+	 * Método que realiza una operación aritmética.
+	 * @param numero1 Valor 1 de la operacion.
+	 * @param numero2 Valor 2 de la operacion.
+	 * @param operacion Tipo de operacion segun los codigos static.
+	 * @return La operacion hecha
+	 * @throws Exception Excepción que salta si introducimos una operacion incorrecta.
+	 */
+	public int operacionAritmetica(int numero1, int numero2, int operacion) 
+		throws Exception {
+		switch(operacion) {
+		case CODIGO_SUMA: return numero1 + numero2;
+		case CODIGO_RESTA: return numero1 - numero2;
+		default: throw new Exception("Operacion Incorrecta");
 		}
-		return c;
-
 	}
+	
+	
 
 }
