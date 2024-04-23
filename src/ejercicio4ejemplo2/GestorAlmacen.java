b9fa4b650dff3c12947d45c4a7a28ffc67c2b889
Mañana <Maana@DESKTOP-RCDFL5S>
2024-04-23 09:30:43 +0200

    Solución final ejemplo2

--- src/ejercicio4ejemplo2/GestorAlmacen.java
+++ src/ejercicio4ejemplo2/GestorAlmacen.java
@@ -4,29 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GestorAlmacen {
-
+	/**
+	 * ArrayList que contiene productos.
+	 */
 	private List<String> productos = new ArrayList<>();
 
-	public void agregarProducto(String producto) {
-		if (!productos.contains(producto)) {
-			productos.add(producto);
-			System.out.println("Producto '" + producto + "' agregado al almacén.");
-		} else {
-			System.out.println("El producto '" + producto + "' ya existe en el almacén.");
-		}
-	}
+	/**
+	 * Atributos estáticos que nos indican el significado de la operación que pongamos.
+	 */
+	public static final int ADD = 0;
+	public static final int REMOVE = 1;
 
-	public void eliminarProducto(String producto) {
-		if (productos.contains(producto)) {
-			productos.remove(producto);
-			System.out.println("Producto '" + producto + "' eliminado del almacén.");
-		} else {
-			System.out.println("El producto '" + producto + "' no existe en el almacén.");
-		}
-	}
+	/**
+	 * Operacion donde se controla que hacer con un producto.
+	 * @param producto El producto que queremos controlar
+	 * @param operacion La operacion que queremos hacer según los static.
+	 * @throws Exception Excepcion que permitirá reconocer los errores que cometamos.
+	 */
 	
-	public void manejarProducto(String producto, boolean agregar) {
+	public void manejarProducto(String producto, int operacion) throws Exception {
 		
+		if (productos.contains(producto) && operacion == REMOVE) {
+			productos.remove(producto);
+			
+		} else if (!productos.contains(producto) && operacion == ADD) {
+			productos.add(producto);
+		/**
+		 * Parte de errores.
+		 */
+			
+		} else if(operacion == ADD) {
+			throw new Exception("No es posible añadir, el producto ya está registrado");
+			
+		} else if(operacion == REMOVE) {
+			throw new Exception("No es posible borrar, el producto no está registrado");
+			
+		} else throw new Exception("No es posible realizar esa opearción");
 	}
-
 }
